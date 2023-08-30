4390092d9fd4db0c7a26fcfb364eb9c11cbe4f72
GoldexDesign <zlajavictory@gmail.com>
2023-08-30 15:21:54 +0200

    tried to fix some errors in Node with npm audit fix --force

---
+++ build/precache-manifest.1da5097376558a44551a7a17e9af94a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddaf9a0db7976e786cc7",
+    "url": "./static/css/main.0f65346b.chunk.css"
+  },
+  {
+    "revision": "ddaf9a0db7976e786cc7",
+    "url": "./static/js/main.ddaf9a0d.chunk.js"
+  },
+  {
+    "revision": "ceffc2895b4f4cfe113d",
+    "url": "./static/js/1.ceffc289.chunk.js"
+  },
+  {
+    "revision": "d8a4fbc9dd1b1bf105e7",
+    "url": "./static/js/2.d8a4fbc9.chunk.js"
+  },
+  {
+    "revision": "6ac3030e71174b791eb6",
+    "url": "./static/js/runtime~main.6ac3030e.js"
+  },
+  {
+    "revision": "2be6d883418d0a7d568480bd7bbf2e28",
+    "url": "./index.html"
+  }
+];
